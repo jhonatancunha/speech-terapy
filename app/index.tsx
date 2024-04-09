65c4d5f4649eec1071d0c696044114106238688d
jhonatancunha <jhocunha1@gmail.com>
2024-04-09 20:11:12 -0300

    feat: adding css

--- app/index.tsx
+++ app/index.tsx
@@ -1,54 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, TouchableHighlight } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { SpeechErrorEvent, SpeechRecognizedEvent, SpeechResultsEvent } from '@react-native-voice/voice';
-import Voice from "@react-native-voice/voice";
-import { Button, Icon, Layout, Spinner, Text } from '@ui-kitten/components';
+import Voice, { SpeechErrorEvent, SpeechResultsEvent } from '@react-native-voice/voice';
+import { Button, Icon, ProgressBar, Spinner, Text } from '@ui-kitten/components';
+import { View } from 'react-native';
 
 
 export default function App() {
-  const [recognized, setRecognized] = useState('');
-  const [volume, setVolume] = useState('');
   const [error, setError] = useState('');
-  const [end, setEnd] = useState('');
   const [results, setResults] = useState([]);
-  const [partialResults, setPartialResults] = useState([]);
-  const [isRecording, setRecording] = useState(false)
+  const [isRecording, setRecording] = useState<boolean>(false)
 
 
-
-  const onSpeechRecognized = (e: SpeechRecognizedEvent) => {
-    console.log('onSpeechRecognized: ', e);
-    setRecognized('√');
-  };
 
 
   const onSpeechError = (e: SpeechErrorEvent) => {
     console.log('onSpeechError: ', e);
     setError(JSON.stringify(e.error));
+    setRecording(false);
   };
 
   const onSpeechResults = (e: SpeechResultsEvent) => {
     console.log('onSpeechResults: ', e);
     setResults(e.value);
+    setRecording(false);
   };
-
-  const onSpeechPartialResults = (e: SpeechResultsEvent) => {
-    console.log('onSpeechPartialResults: ', e);
-    setPartialResults(e.value);
-  };
-
 
   const _startRecognizing = async () => {
     _clearState();
+
     try {
-      if(await Voice.isRecognizing()){
-        _stopRecognizing()
-      }else{
-        console.log('called start');
-        setRecording(true)
-        await Voice.start('pt-BR');
-      }
+      const isRecognizing = await Voice.isRecognizing()
+
+      if(isRecognizing) await _stopRecognizing()
+        
+      console.log('called start');
+      setRecording(true)
+      await Voice.start('pt-BR');
     } catch (e) {
       console.error("start", e);
     }
@@ -65,79 +53,51 @@ export default function App() {
     }
   };
 
-  const _cancelRecognizing = async () => {
-    try {
-      await Voice.cancel();
-      setRecording(false);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const _destroyRecognizer = async () => {
-    try {
-      await Voice.destroy();
-    } catch (e) {
-      console.error(e);
-    }
-    _clearState();
-  };
-
   const _clearState = () => {
-    setRecognized('');
-    setVolume('');
     setError('');
-    setEnd('');
     setResults([]);
-    setPartialResults([]);
     setRecording(false);
   };
 
 
   useEffect(() => {
-    Voice.onSpeechRecognized = onSpeechRecognized;
     Voice.onSpeechError = onSpeechError;
     Voice.onSpeechResults = onSpeechResults;
-    Voice.onSpeechPartialResults = onSpeechPartialResults;
   }, []);
 
   return (
-    <View>
+    <SafeAreaView style={{
+      padding: 20,
+      height: '100%',
+    }}>
+      
+      <ProgressBar progress={0.8}/>
 
-     
-      <Text category="h4">Resultados Parciais</Text>
-      {partialResults.map((result, index) => {
-        return (
-          <Text key={`partial-result-${index}`}>
-            {result}
-          </Text>
-        );
-      })}
-
-
-      <Button onPress={() => {
-        if(isRecording){
-          _stopRecognizing()
-        }else{
-          _startRecognizing()
-        }
-      }} accessoryLeft={<Icon name={isRecording ? "stop-circle-outline" : "mic-outline"} />}>
-        {isRecording ? "Gravando" : "Gravar"}
-      </Button>
-
-      <Text category="h4">Resultados Finais</Text>
-
-      {isRecording  ?  <Spinner status='primary' /> : null}
+      <View style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Text category="h4">G O I A B A</Text>
+      
 
 
-      {results.map((result, index) => {
-        return (
-          <Text key={`result-${index}`}>
-            {result}
-          </Text>
-        );
-      })}
-    </View>
+        {/* <Text category="h4">Resultados Finais</Text> */}
+
+        {isRecording  ?  <Spinner status='primary' /> : null}
+      </View>
+
+
+      {!isRecording ? (
+        <Button 
+          onPress={_startRecognizing} 
+          accessoryLeft={<Icon name={"mic-outline"} />}
+        >
+          Responder
+        </Button>
+      ) : null}
+    </SafeAreaView>
   );
 }
 
